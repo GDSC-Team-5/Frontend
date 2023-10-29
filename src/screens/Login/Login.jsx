8ebc3da783a454267ac2e1c2be88f55e62f8ed7f
youngjin-korea <youngjin1293@naver.com>
2023-10-30 01:33:55 +0900

    feat[#3]:login, signup 페이지 figma 디자인으로 스타일

--- src/screens/Login/Login.jsx
+++ src/screens/Login/Login.jsx
@@ -47,35 +47,37 @@ const Login = () => {
         </div>
         <div className="logcontent">
           <h2>로그인</h2>
+          <div className="input-container-parent">
+            <div className="input-container">
+              <div className="image-div">
+                <img src="/at-sign 2.png" alt="@" />
+              </div>
+              <input
+                className="log-input"
+                type="text"
+                placeholder="이메일"
+                value={loginData.email}
+                onChange={(e) =>
+                  setLoginData({ ...loginData, email: e.target.value })
+                }
+              />
+            </div>
+            <div className="input-container">
+              <div className="image-div">
+                <img src="/lock 1.png" alt="lock" />
+              </div>
+              <input
+                className="log-input"
+                type="password"
+                placeholder="비밀번호"
+                value={loginData.password}
+                onChange={(e) =>
+                  setLoginData({ ...loginData, password: e.target.value })
+                }
+              />
+            </div>
+          </div>
 
-          <div className="input-container">
-            <div className="image-div">
-              <img src="/at-sign 2.png" alt="@" />
-            </div>
-            <input
-              className="log-input"
-              type="text"
-              placeholder="이메일"
-              value={loginData.email}
-              onChange={(e) =>
-                setLoginData({ ...loginData, email: e.target.value })
-              }
-            />
-          </div>
-          <div className="input-container">
-            <div className="image-div">
-              <img src="/lock 1.png" alt="lock" />
-            </div>
-            <input
-              className="log-input"
-              type="password"
-              placeholder="비밀번호"
-              value={loginData.password}
-              onChange={(e) =>
-                setLoginData({ ...loginData, password: e.target.value })
-              }
-            />
-          </div>
           <div>
             <button className="button-submit" onClick={handleSubmit}>
               로그인
